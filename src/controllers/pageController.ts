b78e5a166afb4bf96bfe5437dd5f610f25abbd19
rsddossantos <rsddossantos@gmail.com>
2021-08-26 23:26:52 -0300

    Dinamizando os menus

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,15 +1,39 @@
 import { Request, Response } from 'express';
+import { createMenuObjects } from '../helpers/createMenuObjects';
 
 export const home = (req: Request, res: Response) => {
-    res.send('home no controller');
-    // res.render('pages/page');
+    res.render('pages/page', {
+        menu: createMenuObjects('all'),
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        }
+    });
 }
 export const dogs = (req: Request, res: Response) => {
-    // res.render('pages/page');
+    res.render('pages/page', {
+        menu: createMenuObjects('dog'),
+        banner: {
+            title: 'Cachorros',
+            background: 'banner_dog.jpg'
+        }
+    });
 }
 export const cats = (req: Request, res: Response) => {
-    // res.render('pages/page');
+    res.render('pages/page', {
+        menu: createMenuObjects('cat'),
+        banner: {
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        }
+    });
 }
 export const fishes = (req: Request, res: Response) => {
-    // res.render('pages/page');
+    res.render('pages/page', {
+        menu: createMenuObjects('fish'),
+        banner: {
+            title: 'Peixes',
+            background: 'banner_fish.jpg'
+        }
+    });
 }
