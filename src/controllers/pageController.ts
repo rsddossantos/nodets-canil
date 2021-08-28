e27709a1603949463a7e426f2ded3bb69206ace2
rsddossantos <rsddossantos@gmail.com>
2021-08-27 21:27:48 -0300

    Implementando os models e busca, finalizando o projeto

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,39 +1,56 @@
 import { Request, Response } from 'express';
+import { Pet } from '../models/Pet';
 import { createMenuObjects } from '../helpers/createMenuObjects';
 
 export const home = (req: Request, res: Response) => {
-    res.render('pages/page', {
+    let list = Pet.getAll();
+    let data = {
         menu: createMenuObjects('all'),
         banner: {
             title: 'Todos os animais',
             background: 'allanimals.jpg'
-        }
-    });
+        },
+        list
+    }
+
+    res.render('pages/page', data);
 }
 export const dogs = (req: Request, res: Response) => {
-    res.render('pages/page', {
+    let list = Pet.getFromType('dog');
+    let data = {
         menu: createMenuObjects('dog'),
         banner: {
             title: 'Cachorros',
             background: 'banner_dog.jpg'
-        }
-    });
+        },
+        list
+    }
+
+    res.render('pages/page', data);
 }
 export const cats = (req: Request, res: Response) => {
-    res.render('pages/page', {
+    let list = Pet.getFromType('cat');
+    let data = {
         menu: createMenuObjects('cat'),
         banner: {
             title: 'Gatos',
             background: 'banner_cat.jpg'
-        }
-    });
+        },
+        list
+    }
+
+    res.render('pages/page', data);
 }
 export const fishes = (req: Request, res: Response) => {
-    res.render('pages/page', {
+    let list = Pet.getFromType('fish');
+    let data = {
         menu: createMenuObjects('fish'),
         banner: {
             title: 'Peixes',
             background: 'banner_fish.jpg'
-        }
-    });
+        },
+        list
+    }
+
+    res.render('pages/page', data);
 }
--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ server.engine('mustache', mustache());
 server.use(express.static(path.join(__dirname,'../public')));
 server.use(mainRoutes);
 server.use((req,res)=>{
-    res.send('Página não encontrada!');
+    res.render('pages/404');
 });
 
 server.listen(process.env.PORT, ()=>{
